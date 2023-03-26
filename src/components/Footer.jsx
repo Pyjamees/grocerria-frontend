08ed72bea2f6f84bd69c1fc6fadbbba9eef855c0
GrocerRia <my.grocerria@gmail.com>
2023-03-26 23:41:07 +1100

    test commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,23 @@ import React from "react";
 function Footer() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
-  return <p className="footer">Copyright © {currentYear} GrocerRia</p>;
+  return (
+    <div>
+      <p className="footer">Copyright © {currentYear} GrocerRia</p>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7933047442679365"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style="display:inline-block;width:200px;height:800px"
+        data-ad-client="ca-pub-7933047442679365"
+        data-ad-slot="4232650593"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
+  );
 }
 
 export default Footer;
