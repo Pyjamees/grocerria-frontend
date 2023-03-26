e8e7617e0fbfdb53b6ff8e1cb1507d5517054e8c
GrocerRia <my.grocerria@gmail.com>
2023-03-27 00:36:57 +1100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,6 @@ function Footer() {
   const currentYear = currentDate.getFullYear();
   return (
     <div>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7933047442679365"
-        crossorigin="anonymous"
-      ></script>
-      <ins
-        class="adsbygoogle"
-        style="display:inline-block;width:1200px;height:200px"
-        data-ad-client="ca-pub-7933047442679365"
-        data-ad-slot="4232650593"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       <p className="footer">Copyright © {currentYear} GrocerRia</p>
     </div>
   );
