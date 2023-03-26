cdd0903f8927f40c8799dc6488cf0a952c7f2d32
GrocerRia <my.grocerria@gmail.com>
2023-03-26 22:48:15 +1100

    footer commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
-  return <p className="footer">Copyright © {currentYear} Grocer Ria</p>;
+  return <p className="footer">Copyright © {currentYear} GrocerRia</p>;
 }
 
 export default Footer;
